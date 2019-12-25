19afd45ad9cfd954084d8eba88fe49f8e9119c26
tom hannaford <thom.hannaford@gmail.com>
2019-12-25 22:59:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 def monopoly_with_third_tier
   monopoly = {
 	  :railroads => {:pieces => 4, 
-	  :rent_in_dollars => {:one_piece_owned => "25", :second_piece_owned => "50", :third_piece_owned => "100", :fourth_piece_owned => "200"}, 
+	  :rent_in_dollars => {:one_piece_owned => 25, :second_piece_owned => 50, :third_piece_owned => 100, :fourth_piece_owned => 200}, 
 	  :names => {:owner1 => "", :owner2 => "", :owner3 => "", :owner4 => ""}
 	  }
   }
