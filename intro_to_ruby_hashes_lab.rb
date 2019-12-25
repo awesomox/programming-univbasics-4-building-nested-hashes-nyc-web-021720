0b778e6c532121dbb7e85675006df1155edec3c9
tom hannaford <thom.hannaford@gmail.com>
2019-12-25 22:50:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,13 +5,15 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  	monopoly = {
+  monopoly = {
 	  :railroads => {:pieces => 4}
 	}
 end
 
 def monopoly_with_third_tier
-
+  monopoly = {
+	  :railroads => {:pieces => 4, :rent_in_dollars => ""}
+	}
 end
 
 def monopoly_with_fourth_tier
